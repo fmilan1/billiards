77b62e5d040516bc73ee2b63fb6791984e4579a5
Fazekas Milán <fazekas.milan1@gmail.com>
2026-06-02 22:31:37 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import EightBallMatch from './EightBallMatch'
-import { Home } from './screens/Home'
 import { Admin } from './screens/Admin'
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
-            <Route path='/' Component={Home} />
-            <Route path='/admin' Component={Admin} />
+            <Route path='/' Component={Admin} />
             <Route path='/match' Component={EightBallMatch} />
         </Routes>
     </BrowserRouter>
